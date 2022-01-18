173779730a4b24663379cc9449bdef125d85f4de
flaviosfdev <flaviosf.dev@gmail.com>
2022-01-18 00:12:23 -0300

    Layout tela de transferencia

---
+++ app/src/main/java/com/digitalhouse/dhwallet/TransferFragment.kt
@@ -0,0 +1,9 @@
+package com.digitalhouse.dhwallet
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class TransferFragment : Fragment(R.layout.fragment_transfer)
